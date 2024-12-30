cd03ad0b5dc709605e62c2de08850a868fa72bc4
Maksym Voloshyn <mx.voloshin@gmail.com>
2024-12-30 15:18:48 -0700

    Add mini-pairs

--- init.lua
+++ init.lua
@@ -889,6 +889,9 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      -- Add mini-pairs
+      require('mini.pairs').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
